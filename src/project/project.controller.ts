e46bb1a21d1bf3d81d126e9fc5836d989d16a68b
jsh15932 <jsh1400512@naver.com>
2022-05-25 23:39:00 +0900

    Server:Add Update & Delete Service

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, HttpStatus, Param, Post, Put, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/user/user.entity';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { ProjectService } from './project.service';
 import { Response } from 'express';
+import { ProjectGuard } from './project.guard';
+import { updateProjectDto } from './dto/update-project.dto';
 
 @Controller('project')
 export class ProjectController {
@@ -32,6 +34,50 @@ export class ProjectController {
         });
         if(project) {
             res.status(HttpStatus.OK).send();
+        } else {
+            res.status(HttpStatus.BAD_REQUEST).send();
+        }
+    }
+
+    @Put('update')
+    @UseGuards(AuthGuard('jwt'), ProjectGuard)
+    async update(
+        @Res() res: Response,
+        @Body() updateProjectDto: updateProjectDto
+    ) {
+        const updated = await this.projectService.update(updateProjectDto);
+        if(updated) {
+            res.status(HttpStatus.OK).send();
+        } else {
+            res.status(HttpStatus.BAD_REQUEST).send();
+        }
+    }
+
+    @Post('complete/:id')
+    @UseGuards(AuthGuard('jwt'), ProjectGuard)
+    async completeProject(
+        @Res() res: Response,
+        @Param() params
+    ) {
+        const id = params.id;
+        const completed = await this.projectService.complete(id);
+        if(completed) {
+            const updatedLastEntity = await this.projectService.findById(id);
+            res.status(HttpStatus.OK).send(updatedLastEntity);
+        } else {
+            res.status(HttpStatus.BAD_REQUEST).send();
+        }
+    }
+
+    @Delete(':id')
+    @UseGuards(AuthGuard('jwt'), ProjectGuard)
+    async delete(
+        @Res() res: Response,
+        @Param() params
+    ) {
+        const deleted = await this.projectService.delete(params.id);
+        if(deleted) {
+            res.status(HttpStatus.OK).send(deleted);
         } else {
             res.status(HttpStatus.BAD_REQUEST).send();
         }
